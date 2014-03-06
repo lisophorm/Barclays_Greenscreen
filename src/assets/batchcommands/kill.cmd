eb7152d27f2950792ce76517dffeb2abbdd268b2
lisophorm <lisophorm@gmail.com>
2014-03-06 15:52:43 +0000

    Fixed commands browser

--- src/assets/batchcommands/kill.cmd
+++ src/assets/batchcommands/kill.cmd
@@ -1,3 +1,4 @@
+rem taskkill /IM iexplore.exe /T /f
+rem ping localhost -n 2 > nul
+rem taskkill /IM m2sysplugin.exe /t
 taskkill /IM iexplore.exe /T /f
-ping localhost -n 2 > nul
-taskkill /IM m2sysplugin.exe /t
--- src/assets/batchcommands/launch.cmd
+++ src/assets/batchcommands/launch.cmd
@@ -1,8 +1,11 @@
-ping localhost -n 2 > nul
-taskkill /IM iexplore.exe /t /f
-ping localhost -n 2 > nul
-taskkill /IM m2sysplugin.exe /t
-start iexplore.exe -new "http://192.168.0.150/bio/identify.html"
-ping localhost -n 2 > nul
-start iexplore.exe -new "http://192.168.0.150/bio/register.html"
-ping localhost -n 2 > nul
+rem ping localhost -n 2 > nul
+rem taskkill /IM iexplore.exe /t /f
+rem ping localhost -n 2 > nul
+rem taskkill /IM m2sysplugin.exe /t
+rem start iexplore.exe -new "http://192.168.0.150/bio/identify.html"
+rem ping localhost -n 2 > nul
+rem start iexplore.exe -new "http://192.168.0.150/bio/register.html"
+rem ping localhost -n 2 > nul
+rem taskkill /IM iexplore.exe /t /f
+rem ping localhost -n 5 > nul
+start iexplore.exe -new "http://192.168.0.150/bio"
